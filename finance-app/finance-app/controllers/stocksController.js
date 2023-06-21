759cd0ac1ada922cc2e8630b3b76c66a807c81be
Jennifer Ye <jensome6065@gmail.com>
2023-06-21 15:08:55 -0400

    fixed vulnerabilities

--- finance-app/finance-app/controllers/stocksController.js
+++ finance-app/finance-app/controllers/stocksController.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { MongoClient } = require('mongodb');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database and collection names
-const dbName = 'stockMarketDB';
-const collectionName = 'stocks';
+const Stock = require('../models/stockModel');
 
 // GET /stocks
 router.get('/', async (req, res) => {
   try {
-    // Connect to the MongoDB database
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    // Retrieve stock market data from the database
-    const stocks = await collection.find().toArray();
-
-    // Render the stocks.pug template and pass the stock market data
-    res.render('stocks', { title: 'Stocks Page', stocks });
-
-    // Close the database connection
-    client.close();
+    const stocks = await Stock.find();
+    res.render('stocks', { title: 'Stocks Page', stocks: stocks }); // Pass the stocks array as 'stocks'
   } catch (error) {
-    console.error('Error retrieving stock market data:', error);
+    console.error('Error fetching stocks:', error);
     res.status(500).json({ error: 'Server error' });
   }
 });
