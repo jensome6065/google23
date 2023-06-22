3a577831d4ef7f89b6eeb36cc24ec99c9893ff62
Jennifer Ye <jensome6065@gmail.com>
2023-06-22 16:58:41 -0400

    fixed stock market api

--- finance-app/finance-app/routes/stocks.js
+++ finance-app/finance-app/routes/stocks.js
@@ -1,16 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const StockModel = require('../models/stockModel');
+var request = require('request');
+var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=';
+var api_key = '&apikey=VT2BM0CZYB1E5RKO';
+
+// List of stock symbols
+let symbols = ["IBM", "GOOG", "AAPL", "TSLA"];
+const asyncHandler = require("express-async-handler");
+var stocks = [];
+
+function getDate() {
+  var d = new Date();
+  d.setDate(d.getDate() - 1);
+  // Format date as YYYY-MM-DD
+  let dateFormat = d.getFullYear() + "-" + ((d.getMonth() + 1).length != 2 ?
+      "0" + (d.getMonth() + 1) : (d.getMonth() + 1)) + "-" +
+    (d.getDate().length < 2 ? "0" + d.getDate() : d.getDate());
+  return dateFormat
+}
+
+async function getStocks() {
+  let date = getDate();
+  for (const symbol of symbols) {
+    request.get({
+      url: url + symbol + api_key,
+      json: true,
+      headers: {
+        'User-Agent': 'request'
+      }
+    }, (err, res, data) => {
+
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        var json_data = data["Time Series (Daily)"][date]
+        json_data.symbol = symbol;
+        stocks.push(json_data);
+        // data is successfully parsed as a JSON object:
+        console.log(date);
+        console.log(data["Time Series (Daily)"][date]);
+      }
+    });
+  }
+}
+getStocks();
 
 router.get('/', async (req, res) => {
-  try {
-    const stocks = await StockModel.find();
-
-    res.render('stocks', { title: 'Stocks Page', stocks }); 
-  } catch (error) {
-    console.error('Error fetching stock market data:', error);
-    res.status(500).json({ error: 'Server error' });
-  }
+  console.log("stocks: ");
+  console.log(stocks);
+  res.render('stocks', {
+    title: 'Stocks Recap For ' + getDate(),
+    stocks: stocks,
+  }); // Use the variable name "stocks"
 });
 
 module.exports = router;
